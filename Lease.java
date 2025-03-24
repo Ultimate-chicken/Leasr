fcd3784556d7b1328449896cbbc7ed877bf9b517
Ultimate-chicken <55349969+Ultimate-chicken@users.noreply.github.com>
2025-03-24 22:15:22 +0000

    Update Lease.java

--- Lease.java
+++ Lease.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import javax.swing.JOptionPane;
 
 /**
  * Write a description of class Lease here.
@@ -7,14 +6,25 @@ import javax.swing.JOptionPane;
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Lease extends RecurringContract
-{
+public class Lease extends Contract {
+    private double monthlyLeaseCost;
+    private int leaseDuration;
+    private double depositAmount;
 
-    private double leaseMonthlyCost;
-    private boolean leaseActive;
-
-    public Lease(String contractID, ArrayList<Product> productSelection, Account linkedAccount)
-    {
+    public Lease(String contractID, ArrayList<Product> productSelection, Account linkedAccount, 
+    double monthlyLeaseCost, int leaseDuration, double depositAmount) {
         super(contractID, productSelection, linkedAccount);
+        this.monthlyLeaseCost = monthlyLeaseCost;
+        this.leaseDuration = leaseDuration;
+        this.depositAmount = depositAmount;
+    }
+
+    @Override
+    public String toString() {
+        return "Lease " + super.toString() +
+        "\nMonthly Lease Cost: " + monthlyLeaseCost + "€" +
+        "\nLease Duration: " + leaseDuration + " months" +
+        "\nDeposit Amount: " + depositAmount + "€";
     }
 }
+
