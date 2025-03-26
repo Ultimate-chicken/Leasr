23e3dff2cefd3f3dfb4e08508e9b194db5cf7f81
nzp3 <138122947+nzp3@users.noreply.github.com>
2025-03-26 23:02:48 +0000

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,14 +1,7 @@
 import java.util.ArrayList;
-import javax.swing.JOptionPane;
-import java.util.HashMap;
 
-//for features, consider using sets instead of arrayList because it doesn't allow for duplicates. 
-//Adding elements to set: set.add(). Relate to input. 
-//Ok so for the features array, create 2 parallel arrays: 1 with features, other with content. Relate content to index in features. 
-
-/**
- * Write a description of class Product here.
- *
+/** This class contains fields and methods regarding products that are included in leases. The structure is basic. It includes constructors,
+ * 
  * @author (your name)
  * @version (a version number or a date)
  */
@@ -21,7 +14,7 @@ public class Product
     private int productStock;
     private double productBasePrice;
     private boolean productStatus;
-    
+
     /** Constructor for objects of class Product */
     public Product(String productID, String productName, String productDescription, String productSupplier, int productStock,
     double productBasePrice)
@@ -33,81 +26,80 @@ public class Product
         this.productStock = productStock;
         this.productBasePrice = productBasePrice;
         this.productStatus = true;
-        
+
         System.out.println("Product Creation successfull!\n");
         System.out.println(this.toString());
         System.out.println("\n\n");
     }
-    
+
     //WORKS
     @Override
     public String toString() {
-        return "Product Details:\n" +
-           "ID: " + this.getProductID() + "\n" +
-           "Name: " + this.getProductName() + "\n" +
-           "Description: " + this.getProductDescription() + "\n" +
-           "Supplier: " + this.getProductSupplier() + "\n" +
-           "Stock: " + this.getProductStock() + "\n" +
-           "Base Price: " + String.format("%.2f€", this.getProductBasePrice()) + "\n" +
-           "Status: " + this.getProductStatus();
-}
-    
+        return "\nID: " + this.getProductID() + 
+        " Name: " + this.getProductName() +
+        " Description: " + this.getProductDescription() +
+        " Supplier: " + this.getProductSupplier() + 
+        " Stock: " + this.getProductStock() + 
+        " Base Price: " + String.format("%.2f€", this.getProductBasePrice()) +
+        " Status: " + this.getProductStatus();
+    }
+
     //getters
     public String getProductID() {
-    return productID;
-}
+        return productID;
+    }
 
-public String getProductName() {
-    return productName;
-}
+    public String getProductName() {
+        return productName;
+    }
 
-public String getProductDescription() {
-    return productDescription;
-}
+    public String getProductDescription() {
+        return productDescription;
+    }
 
-public String getProductSupplier() {
-    return productSupplier;
-}
+    public String getProductSupplier() {
+        return productSupplier;
+    }
 
-public int getProductStock() {
-    return productStock;
-}
+    public int getProductStock() {
+        return productStock;
+    }
 
-public double getProductBasePrice() {
-    return productBasePrice;
-}
+    public double getProductBasePrice() {
+        return productBasePrice;
+    }
 
-public boolean getProductStatus() {
-    return productStatus;
-}
+    public boolean getProductStatus() {
+        return productStatus;
+    }
 
-//setters
-public void setProductID(String productID) {
-    this.productID = productID;
-}
+    //setters
+    public void setProductID(String productID) {
+        this.productID = productID;
+    }
 
-public void setProductName(String productName) {
-    this.productName = productName;
-}
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
 
-public void setProductDescription(String productDescription) {
-    this.productDescription = productDescription;
-}
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
 
-public void setProductSupplier(String productSupplier) {
-    this.productSupplier = productSupplier;
-}
+    public void setProductSupplier(String productSupplier) {
+        this.productSupplier = productSupplier;
+    }
 
-public void setProductStock(int newStock) {
-    this.productStock = newStock;
-}
+    public void setProductStock(int newStock) {
+        this.productStock = newStock;
+    }
 
-public void setProductBasePrice(double productBasePrice) {
-    this.productBasePrice = productBasePrice;
-}
+    public void setProductBasePrice(double productBasePrice) {
+        this.productBasePrice = productBasePrice;
+    }
 
-public void setProductStatus(boolean productStatus) {
-    this.productStatus = productStatus;
-}
+    public void setProductStatus(boolean productStatus) {
+        this.productStatus = productStatus;
+    }
 
 }
